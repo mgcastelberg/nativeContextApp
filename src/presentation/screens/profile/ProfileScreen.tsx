153739ff8bdc72f256d7bd990bd2d8a1a07c193c
Manuel Gomez <jmgc@virket.com>
2024-04-28 21:32:14 -0600

    Cambios en el Store

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 import { styles } from '../../../config/app-theme'
 import { useProfileStore } from '../../store/profile-store'
 
@@ -7,11 +7,22 @@ export const ProfileScreen = () => {
 
   const name = useProfileStore( state => state.name );
   const email = useProfileStore( state => state.email );
+  const changeProfile = useProfileStore( state => state.changeProfile );
 
   return (
     <View style={ styles.container }>
-        <Text style={ styles.title }>{ name }</Text>
-        <Text style={ styles.title }>{ email }</Text>
+      <Text style={ styles.title }>{ name }</Text>
+      <Text style={ styles.title }>{ email }</Text>
+      <Pressable onPress={ () => useProfileStore.setState({ name: 'Manuel Gómez'})} style={ styles.primaryButton }>
+        <Text>Cambiar nombre</Text>
+      </Pressable>
+      <Pressable onPress={ () => useProfileStore.setState({ email: 'jmgc@virket.com'})} style={ styles.primaryButton }>
+        <Text>Cambiar email</Text>
+      </Pressable>
+
+      <Pressable onPress={ () => changeProfile('Jhon Doe', 'jhon.doe@test.com') } style={ styles.primaryButton }>
+        <Text>Regresar a Jhon Doe</Text>
+      </Pressable>
     </View>
   )
 }
--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import { styles } from '../../../config/app-theme'
+import { useProfileStore } from '../../store/profile-store';
 
 export const SettingsScreen = () => {
+  const name = useProfileStore( state => state.name );
+  const email = useProfileStore( state => state.email );
+
   return (
     <View style={ styles.container }>
-        <Text>SettingsScreen</Text>
+        <Text style={ styles.title }>{ name }</Text>
+        <Text style={ styles.title }>{ email }</Text>
     </View>
   )
 }
